c09a4dee677d0e717f625091540613dadb1a9c86
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-07-23 15:34:27 -0400

    refactor chap4 ex3 with case statement without arg

--- flow_control_chap_4/exercise_3.rb
+++ flow_control_chap_4/exercise_3.rb
@@ -1,10 +1,11 @@
 puts "Give number between 0 and 100"
 num = gets.chomp.to_i
 
-if num >= 0 && num <= 50
-  puts "#{num} is between 0 and 50"
-elsif num >= 51 && num <= 100
-  puts "#{num} is between 50 and 100"
-else
-  puts "#{num} is above 100 or below 0"
+case
+  when num >= 0 && num <= 50
+    puts "#{num} is between 0 and 50"
+  when num >= 51 && num <= 100
+    puts "#{num} is between 50 and 100"
+  else
+    puts "#{num} is above 100 or below 0"
 end
