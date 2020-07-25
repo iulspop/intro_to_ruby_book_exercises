8df4a16baa685dafae98ee689457cd9fe2ab8628
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-07-25 18:06:15 -0400

    add solution chap2 ex2

---
+++ variables_chap_2/age.rb
@@ -0,0 +1,32 @@
+puts "How old are you??!!?!?!?"
+age = gets.chomp.to_i
+
+puts "In 10 years you will be:"
+puts "#{age + 10} years old"
+
+puts "In 20 years you will be:"
+puts "#{age + 20} years old"
+
+puts "In 30 years you will be:"
+puts "#{age + 30} years old"
+
+puts "In 40 years you will be:"
+puts "#{age + 40} years old"
+
+puts "In 50 years you will be:"
+puts "#{age + 50} years old"
+
+puts "In 60 years you will be:"
+puts "#{age + 60} years old"
+
+puts "In 70 years you will be:"
+puts "#{age + 70} years old"
+
+puts "In 80 years you will be:"
+puts "#{age + 80} years old"
+
+puts "In 90 years you will be:"
+puts "#{age + 90} years old"
+
+puts "In 100 years you will be:"
+puts "Likely 'Passed away'"
