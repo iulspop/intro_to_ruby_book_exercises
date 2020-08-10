c6ff0a0f63b29b1444efce5e7a5ccbd120c6415a
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-08-10 15:05:08 -0400

    play with loops and for loop

---
+++ loops_iterator_chap_5/drill_loops.rb
@@ -0,0 +1,37 @@
+array = ["The","Simplest","Way","Is","Your","Mind","."]
+hash = {
+  joy: "beauty",
+  concentration: "serenity",
+  virtue: "wisdom",
+  exalted: "limitless"
+}
+
+for index in 1..array.length do
+  puts "#{array.reverse[index]}"
+end
+
+# loop do
+#   next
+#   break
+# end
+
+# while condition do
+
+# end
+
+
+# loop do
+
+#   condition
+# end
+
+# until condition do # like while !condition
+
+# end
+
+# for i in 1..5 do
+# end
+
+
+# # iterators
+# # each, map...
